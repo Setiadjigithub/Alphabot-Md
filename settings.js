0a0d00a50cf643963bb97486df9c21a9047cfa82
Setiadjigithub <112676354+Setiadjigithub@users.noreply.github.com>
2022-10-06 08:05:42 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,11 +7,11 @@ global.APIs = {
 }
 
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'V12',
 }
 
 // setting 
-global.autoread = false // auto read message
+global.autoread = false
 global.autobio = true 
 global.userRegister = false 
 global.autoblok212 = true
@@ -24,25 +24,25 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Alphabot-Mdོ"
+global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.myweb ="https://api.zeeoneofc.xyz"
+global.youtube = "https://youtube.com/c/ZeeoneOfc"//
+global.myweb2 = "https://linktr.ee/zeeoneofc"
+global.email = "zeeoneofc@gmail.com"
 global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6281296382470","6285727501561"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281296382470" // nomor wa kalian
-global.ownernomerr = "+6281296382470" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.timezone = 'Asia/Jakarta'
+global.premium = ['62887435047326']
+global.owner = ["6281296382470","6285727501561"]
+global.ownernomer = "6281296382470"
+global.ownernomerr = "+6281296382470"
+global.thumbnail = "file:///storage/0403-0201/Android/data/com.android.chrome/files/Download/images%20-%202022-10-06T075428.718.jpeg"
+global.donasi = "./image/donasi.jpg"
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg"/
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
-global.sessionName = 'session'
+global.packname = '© Alphabot-Mdོ'
+global.author = 'Di Buat Oleh Setiadji'
+global.sessionName = 'session.data.json'
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
